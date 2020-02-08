79358d03c5ee7f003c1f8a17fa0c008555383ac0
Ahmed Hatem <ahmedhatembekaar@gmail.com>
2020-02-08 21:24:58 +0200

    Added Placeholder - Segment - table component

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.scss']
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: "Ahmed", age: 24, job: "Front end developer" },
+    { name: "Mohamed", age: 25, job: "Back end developer" },
+    { name: "Ali", age: 22, job: "Designer" }
+  ];
 
+  headers = [
+    { key: "name", label: "Name" },
+    { key: "age", label: "Age" },
+    { key: "job", label: "Job" }
+  ];
   constructor() { }
 
   ngOnInit() {
--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -1,14 +1,20 @@
+import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import { TableComponent } from './table/table.component';
+import { CamerasComponent } from './categories/cameras/cameras.component';
+import { BooksComponent } from './categories/books/books.component';
+import { FurnitureComponent } from './categories/furniture/furniture.component';
 
 
 @NgModule({
-  declarations: [CollectionsHomeComponent],
+  declarations: [CollectionsHomeComponent, TableComponent, CamerasComponent, BooksComponent, FurnitureComponent],
   imports: [
     CommonModule,
+    SharedModule,
     CollectionsRoutingModule
   ]
 })
