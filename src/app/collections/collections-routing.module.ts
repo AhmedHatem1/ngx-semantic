756a9133c6baac1e7873a68232dd11cedf4836d4
Ahmed Hatem <ahmedhatembekaar@gmail.com>
2020-02-08 18:43:49 +0200

    split project into modules

---
+++ src/app/collections/collections-routing.module.ts
@@ -0,0 +1,14 @@
+import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  { path: "", component: CollectionsHomeComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CollectionsRoutingModule { }
